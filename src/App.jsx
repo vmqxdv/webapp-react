befe15f0dd1ee460c3dde1b44cb0e69f16346e8f
vmqxdv <vquintilio@gmail.com>
2025-05-12 22:29:24 +0200

    feat: Implementato routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MovieDetail from './pages/MovieDetail';
+
 export default function App() {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies/:id' element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
