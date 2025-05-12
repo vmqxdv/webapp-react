6158261d60dae439c686600bd2f8470eeed8ae01
vmqxdv <vquintilio@gmail.com>
2025-05-12 22:29:56 +0200

    feat: Aggiunto componente MovieDetail

---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+export default function MovieDetail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:1229/movies/${id}`)
+      .then((response) => setMovie(response.data))
+      .catch((error) => console.error('Errore nel recupero del film:', error));
+  }, [id]);
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <div>{movie.description}</div>
+    </div>
+  );
+};
