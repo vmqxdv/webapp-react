7242a34448f4672f11b8cf96c6b91c517e7b5b13
vmqxdv <vquintilio@gmail.com>
2025-05-13 17:40:56 +0200

    style: Diviso in colonne le cards

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,15 @@
 import MovieItem from './MovieItem';
 
 export default function MovieList({ movies }) {
-  const MovieList = movies.map((movie) => (
-    <MovieItem key={movie.id} movie={movie} />
+  const movieList = movies.map((movie) => (
+    <div className='col' key={movie.id}>
+      <MovieItem movie={movie} />
+    </div>
   ));
 
   return (
-    <ul>
-      {MovieList}
-    </ul>
+    <div className='row row-cols-1 row-cols-md-3 g-4 mb-5'>
+      {movieList}
+    </div>
   );
-}
+};
